3f176d869b5d6085b26e803e05b3ca3908041a1c
dawit101 <>
2023-04-06 00:45:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+#My first ever python code
+
+name = input('what is your name?')
+print('heeeelllllooooo ' + name) #make sure to add space between last letter and ' so it will have a space between last letter and Dawit
